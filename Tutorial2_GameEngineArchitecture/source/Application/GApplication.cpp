0b2fc9f2273306537e631a8d7ce49086dd507418
PardCode <45696173+PardCode@users.noreply.github.com>
2021-07-20 16:05:25 +0200

    Tutorial2
    Tutorial2

---
+++ Tutorial2_GameEngineArchitecture/source/Application/GApplication.cpp
@@ -0,0 +1,60 @@
+/*Ground Engine (https://www.pardcode.com/groundengine)
+
+Copyright (c) 2021, PardCode All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+	1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+	2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.*/
+
+#include "GroundEngine/Application/GApplication.h"
+#include <stdexcept>
+using namespace ground;
+
+
+GApplication* GApplication::m_instance = nullptr;
+
+
+ground::GApplication::GApplication()
+{
+	if (!m_instance)
+		m_instance = this;
+	else
+		throw std::runtime_error("GApplication already created.");
+}
+
+ground::GApplication::~GApplication()
+{
+}
+
+void ground::GApplication::run()
+{
+	onInit();
+
+	while (m_isRunning)
+	{
+
+
+	}
+
+	onQuit();
+}
+
+void ground::GApplication::quit()
+{
+	m_isRunning = false;
+}
+
+GApplication* ground::GApplication::get()
+{
+	return m_instance;
+}
